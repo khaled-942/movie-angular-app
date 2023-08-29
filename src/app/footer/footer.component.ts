ef61629b881222f89a2c03e8248ae45e76f986a1
khaled <khaledmohamed942@gmail.com>
2023-08-30 00:12:01 +0200

    revision

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  myName: string = 'khaled'
+  imgSrc:string= "https://images.unsplash.com/photo-1612441804231-77a36b284856?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW91bnRhaW4lMjBsYW5kc2NhcGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+  test:string ='khaled'
+  test1:number=5
+  arr:Array<string>=['k','m','A','s']
+  sayHello(){
+    alert(`hello${this.myName}`)
+  }
   constructor() { }
 
   ngOnInit(): void {
