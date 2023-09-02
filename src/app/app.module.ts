ee2138011866f7b8b6feba784ba88238fb894080
khaled <76748059+khaled-942@users.noreply.github.com>
2023-09-02 16:56:10 +0300

    last v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
@@ -23,12 +23,13 @@ import { CurrencyPipe } from './currency.pipe';
     AbouUsComponent,
     CreatMovieComponent,
     MovieDetailsComponent,
-    CurrencyPipe
+    CurrencyPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/creat-movie/creat-movie.component.ts
+++ src/app/creat-movie/creat-movie.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-creat-movie',
   templateUrl: './creat-movie.component.html',
-  styleUrls: ['./creat-movie.component.scss']
+  styleUrls: ['./creat-movie.component.scss'],
 })
 export class CreatMovieComponent implements OnInit {
-  constructor() { }
-  model:any
-  submitMovie(values:any){
-    console.log(values)
-  }
-  ngOnInit(): void {
+  moviesReactiveForm: FormGroup | any;
+  constructor(private fb: FormBuilder) {}
+
+  submitMovie(values: any) {
+    console.log(values);
   }
 
+  reactiveSubmit() {
+    console.log(this.moviesReactiveForm.controls.movieReactiveName)
+  }
+  get controls(){
+    return this.moviesReactiveForm.controls;
+  }
+  ngOnInit(): void {
+    this.moviesReactiveForm = this.fb.group({
+      movieReactiveName:[null,[Validators.required,Validators.minLength(3)]]
+    })
+    // this.moviesReactiveForm = new FormGroup({
+    //   movieReactiveName: new FormControl(null, [Validators.required]),
+    // });
+  }
 }
